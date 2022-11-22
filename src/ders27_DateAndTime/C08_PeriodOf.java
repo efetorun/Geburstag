3ceb91bb2163e33fd83c707e1b7911b265e04ee8
efe5561 <egzy5561@gmail.com>
2022-11-23 00:47:14 +0100

    Wie alt bin ich(Jahre/Monat/Tag)

--- src/ders27_DateAndTime/C08_PeriodOf.java
+++ src/ders27_DateAndTime/C08_PeriodOf.java
@@ -1,4 +1,20 @@
 package ders27_DateAndTime;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+
 public class C08_PeriodOf {
+    public static void main(String[] args) {
+
+
+    //dogumdan bugüne nekadar zaman gecti
+
+    LocalDate bugun= LocalDate.now();
+    LocalDate dogumgunu=LocalDate.of(1990,12,26);
+
+    Period gecenSure= Period.between(dogumgunu,bugun);
+        System.out.println(gecenSure);
+
+}
 }
